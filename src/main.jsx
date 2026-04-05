c47bd8e81185938c6503217d26084d5f5dedb14b
dglink25 <dglink25@gmail.com>
2026-04-05 07:57:37 +0100

    Suppression Correction responsivité

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './responsive.css'
 import './i18n/index.js'   // ← Initialise i18next AVANT le rendu
 import App from './App.jsx'
 
