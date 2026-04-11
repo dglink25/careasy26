c8f3b81621978a6db7d8412db72b0a07910e6c7b
dglink25 <dglink25@gmail.com>
2026-04-11 18:45:07 +0100

    Correction bugs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-
-
-
 
